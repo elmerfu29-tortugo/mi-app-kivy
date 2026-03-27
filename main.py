d4f6c29197b1231c96fa6d5d3959934f7ed9f0b8
elmerfu29-tortugo <elmerfu.29@gmail.com>
2026-03-26 20:54:30 -0600

    Mi primera app

---
+++ main.py
@@ -0,0 +1,18 @@
+﻿import os
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.clock import Clock
+
+class MinimalApp(App):
+    def build(self):
+        self.label = Label(text="Iniciando...", font_size=40)
+        Clock.schedule_once(self.cambiar_texto, 2)
+        return self.label
+    
+    def cambiar_texto(self, dt):
+        self.label.text = "✅ FUNCIONA!\nApp correcta"
+
+if __name__ == '__main__':
+    MinimalApp().run()
